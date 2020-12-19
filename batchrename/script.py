8281757d2af6c2b4d67264a8a92e0ef63ae08662
Colt Baldridge <coltbaldridge@gmail.com>
2020-12-18 20:12:59 -0600

    Created a new file where the program will run.

---
+++ batchrename/script.py
@@ -0,0 +1,15 @@
+# This is where the script will eventually run.
+# While batchrename.py is the main program,
+#   its components will be reworked and distributed
+#   to more appropriate files.
+
+import directory_manager
+import setup
+# import queue
+
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
