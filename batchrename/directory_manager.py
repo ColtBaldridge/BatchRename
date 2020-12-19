61e091cc9ad82c708442a9450ad421a80e889f3d
Colt Baldridge <coltbaldridge@gmail.com>
2020-12-18 20:23:52 -0600

    Added the following: 1. Created a constant, ROOT, to represent the programs's install directory. 2. Revised __init__ to prompt the user for the working directory. 3. fleshed out hard_reset(). 4. Created a move() method, subject to removal. 5. Since __init__ now defines the working directory, set_target is now obselete.

---
+++ batchrename/directory_manager.py
@@ -0,0 +1,38 @@
+import os
+# from pathlib import Path
+from shutil
+
+
+# This is the program's install location.
+ROOT = os.path.realpath(__file__)
+
+
+class DirManager:
+
+    def __init__(self):
+        # Target is where the files live.
+        print('Where are your files located?')
+        self.target = input('>>> ')
+        pass
+    
+    '''
+    def set_target(self, path):
+        os.chdir(path)
+        pass
+    '''
+
+    def get_files(self, path):
+        '''Returns the target file path'''
+        return os.listdir(path)
+
+    def hard_reset(self, self.target, backup, missed_files):
+        '''In case of catastrophic failure, break glass.'''
+        os.remove(self.target)
+        os.remove(missed_files)
+        os.mkdir(missed_files)
+        shutil.copy2(backup, self.target)
+        pass
+
+    def move(self, pdf, directory):
+        move(pdf, directory)
+        pass
