e1dbe593acd60406a7014a7affec5cc2e81eb2ad
Colt Baldridge <coltbaldridge@gmail.com>
2020-12-22 00:28:40 -0600

    Further refine the module's methods: * Eliminate uncecessary class attriutes * Rename self.directory to self.path * Create self.files to store the files to be edited * Create contents() method to populate self.files * Changed create() method to a private scope * Automatically create the directory by calling __create() in the constructor * Decluttered methods by importing specific modules from os

--- batchrename/directory.py
+++ batchrename/directory.py
@@ -1,4 +1,6 @@
-import os
+from os import mkdir
+from os import listdir
+from os import path
 from shutil import rmtree
 
 
@@ -6,16 +8,28 @@ class Directory:
 
     def __init__(self, root, directory):
         '''Create a directory at the given path.'''
-        self.directory = str(os.path.join(root, directory))
-        os.mkdir(self.directory)
+        self.path = str(path.join(root, directory))
+        self.files = []
 
-    def create(self, path):
+        self.__create(self.path)
+
+    def __create(self, path):
         '''Use this only if something has gone wrong.'''
-        if os.path.exists(path) == False:
-            os.mkdir(path)
+        if path.exists(path) == False:
+            mkdir(path)
         else:
-            print(f'A folder named {os.path.basename(path)} already exists.')
+            print(f'A folder named {path.basename(path)} already exists.')
 
     def delete(self, path):
         '''Delete the given directory.'''
         rmtree(path)
+
+    # Perhaps this should go in the main script.
+    def contents(self, path, format=None):
+        for entry in os.listidr(self.path):
+            if path.isdir(entry):
+                continue
+            else:    
+                if entry[entry.index('.') + 1:] == str(format):
+                    self.files.append(entry)
+        
