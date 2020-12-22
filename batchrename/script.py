19917ccecbb98bca97c5b198357165742fb9d65b
Colt Baldridge <coltbaldridge@gmail.com>
2020-12-22 00:36:12 -0600

    Begin implementing main script executions: * Added relevant module imports * Created a setup() function to render setup.py obselte * Moved ROOT constant to script.py. * Implemented two ways to set ROOT

--- batchrename/script.py
+++ batchrename/script.py
@@ -1,15 +1,34 @@
-# This is where the script will eventually run.
-# While batchrename.py is the main program,
-#   its components will be reworked and distributed
-#   to more appropriate files.
+import os
+from PyPDF2 import PdfFileReader
+from directory import Directory
+import shutil
+from sys import modules
 
-import directory_manager
-import setup
-# import queue
+def setup():
+    '''
+    Required modules:
+        os
+        pathlib
+        shutil
+        PyPDF2
+    '''
+    pass
+
+print('Enter the directory path to the target files.')
+ROOT = str(input('>>> '))
+
+# If the script ends up being a single file use this line instead.
+ROOT = os.path.realpath(__file__)
 
 
 def main():
-    pass
+    
+    backup = Directory(ROOT, 'Backup')
+    missed = Directory(ROOT, 'Missed Files')
+
+    # This exists solely to shut up the interpreter.
+    print(backup + missed)
+
 
 if __name__ == "__main__":
     main()
