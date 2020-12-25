c12648cbdb09bc286629b2c6b7b11a9e1e924df3
Colt Baldridge <coltbaldridge@gmail.com>
2020-12-25 15:44:01 -0600

    Consolidate project files.
    Things may have gotten a little out of hand. Redesigns and testing led to
    project instability. Old versions of code lingered long after their use. New
    iterations of the same classes took there place. In turn, those too were made
    obselete.
    
    Such are the fuits of little planning. This process is a valuable, if not
    frustrating, lesson. I'll be taking some time to iron out the design.

--- batchrename/directory.py
+++ batchrename/directory.py
@@ -1,44 +1,52 @@
-import os
-from shutil import rmtree
+from os import path
+from os import listdir
+from os import scandir
+from PyDPF2 import PdfFileReader
+from shutil import copy2
 
 
 class Directory:
-    
-    def __init__(self, root, directory):
-        '''Create a directory at the given path.'''
-        self.path = os.path.join(root, directory)
-        self.name = directory
-        self.files = []
-        self.exceptions = []
+
+
+    class File:
         
+
+        def __init__(self, name, path):
+            self.name = name
+            self.extension name[name.rindex('.') + 1:]
+            self.metadata = {}
+            self.path = path
+
+        def scrape(self):
+            with open(self.name, 'rb') as f:
+                try:
+                    self.metadata = PdfFileReader(f).getDocumentInfo()
+                except:
+                    pass
+                    # Either use move() method or shutil.copy2()
+        
+        def format(self):
+            self.name.replace(':', ' -')
+
+
+    def __init__(self, root, name):
+        self.name = name
+        self.path = path.listdir(root, name)
+        self.queue = []
+
         self.__create()
 
     def __create(self):
-        '''Use this only if something has gone wrong.'''
-        if not os.path.exists(self.name):
-            os.mkdir(self.name)
-        else:
-            print(f'Found existing folder {self.name}.')
-    
-    def __get_extension(self, file): 
-        # Look into implementing str.rindex() for files with periods in their title.
-        return file[file.index('.') + 1:]
+    '''Use this only if something has gone wrong.'''
+    if not os.path.exists(self.name):
+        os.mkdir(self.name)
+    else:
+        print(f'Found existing folder {self.name}.')
 
     def delete(self):
         '''Delete the given directory.'''
         rmtree(self.path)
 
-    # Perhaps this should go in the main script.
-    def contents(self, format=None):
-        for entry in os.listdir(self.name):
-            if not os.path.isdir(os.path.join(self.path, entry)):
-                # print(f'Fetching file exention of {entry}')
-                try:
-                    if self.__get_extension(entry) == str(format):
-                        self.files.append(entry)
-                    else:
-                        self.exceptions.append(entry)
-                except:
-                    print(f'\nSubstring not found for {entry} (index: {os.listdir(self.name).index(entry)})')
-            else:
-                self.exceptions.append(entry)
+    def load_queue(self):
+        for entry in scandir():
+            if entry.is_file:
