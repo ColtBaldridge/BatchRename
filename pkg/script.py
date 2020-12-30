9bfe5aef57786ba39138efd6b1398d1a6edba140
Colt Baldridge <coltbaldridge@gmail.com>
2020-12-30 12:56:13 -0600

    Add docstrings to Entry and File methods.

--- pkg/script.py
+++ pkg/script.py
@@ -4,34 +4,60 @@ import shutil
 
 
 
-class File:
+class Entry:
     
+    class File(Entry):
+
+            def __init__(self, name):
+                '''Initialize an entry which is a file.
+
+                Attributes:
+                extension -- represents the file's format extension
+                metadata -- dictionary which holds the file's metadata
+                '''
+                self.extension = name[name.rindex('.') + 1:]
+                self.metadata = {}
+
+            def __format(self, name):
+                '''Replace characters unusable in the file system.'''
+                return name.replace(':', ' -')
+
+            def scrape(self):
+                '''Extract PDF metadata for storage.'''
+                with open(self.name, 'rb') as f:
+                    try:
+                        self.metadata = PdfFileReader(f).getDocumentInfo()
+                    except:
+                        print(f'Metadata import error [SCRAPE]: {self.name}')
+
+            def rename(self, backup_path):
+                '''Replace the original name with scraped metadata.'''
+                try:
+                    formatted_name = format(self.metadata['/Title'])
+                    os.rename(self.name, formatted_name)
+                except KeyError:
+                    print(f'Metadata import error [RENAME]: {self.name}')
+                    self.__move(backup_path)
 
     def __init__(self, name, path):
+        '''Initialize an object representing an entry in a directory.
+        
+        Attributes:
+        name -- the entry's name
+        path -- the real file path, starting from the working directory
+        '''
         self.name = name
-        self.extension = name[name.rindex('.') + 1:]
-        self.metadata = {}
         self.path = path
+
+        self.__create()
     
-    def __format(self, name):
-        return name.replace(':', ' -')
+    def __create(self):
+        '''Create a new directory with the same name as the object name. '''
+        os.mkdir(self.name)
 
-    def scrape(self):
-        with open(self.name, 'rb') as f:
-            try:
-                self.metadata = PdfFileReader(f).getDocumentInfo()
-            except:
-                print(f'Metadata import error [SCRAPE]: {self.name}')
-
-    def rename(self, backup_path):
-        try:
-            formatted_name = format(self.metadata['/Title'])
-            os.rename(self.name, formatted_name)
-        except KeyError:
-            print(f'Metadata import error [RENAME]: {self.name}')
-            self.__move(backup_path)
 
     def __move(self, dst_path):
+        '''Move the object to a new directory.'''
         shutil.copy2(self.path, dst_path)
 
 
@@ -45,7 +71,7 @@ def main():
 
     for entry in os.scandir():
         if entry.is_file():
-            # objectFile.scrape()
+            q.put(File(entry.name, entry.path))
             pass
 
 
