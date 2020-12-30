af25b8542091078434a9e10eb1d30211d071c830
Colt Baldridge <coltbaldridge@gmail.com>
2020-12-30 16:33:41 -0600

    Creat a script to automate resetting test folder

---
+++ pkg/tests/restore.py
@@ -0,0 +1,9 @@
+# This script resets the repository through the following:
+# 
+# 1) Delete reading-list
+# 2) Make a copy of reading-list-backup
+# 3) Rename reading-list-backup (copy) to reading-list
+# 
+# Alternatively:
+# 1) Delete all enetries at reading-list/*
+# 2) copy reading-list-backup reading-list
