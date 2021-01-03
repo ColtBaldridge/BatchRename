80b38f10020b2fa3845ac798d804cf322a9d577e
Colt Baldridge <coltbaldridge@gmail.com>
2021-01-03 14:36:42 -0600

    Add functionality to restore.py

--- pkg/tests/restore.py
+++ pkg/tests/restore.py
@@ -7,3 +7,47 @@
 # Alternatively:
 # 1) Delete all enetries at reading-list/*
 # 2) copy reading-list-backup reading-list
+
+
+from pathlib import Path
+from os import getcwd
+from os import mkdir
+from os import path
+from os import scandir
+from shutil import copytree
+from shutil import rmtree
+import time
+
+
+ROOT = Path(getcwd())
+
+
+def main():
+
+    # Constants
+    production = 'reading-list'
+    backup = 'reading-list-backup'
+
+    # Lambdas
+    verify_contents = lambda src, dst : scandir(src) == scandir(dst)
+
+    print('Restoring PDF directory from the backup...')
+    rmtree(production)
+    copytree(backup, production)
+
+    time.sleep(0.5)
+
+    print('Checking the restoration validity...', end='')
+    if verify_contents(backup, production):
+        print('Success!')
+    else:
+        print('Coppy Error')
+        # print('Initiating scan...')
+        # 
+        # time.sleep(0.5)
+        # 
+        # for         
+
+
+if ROOT.name == 'pkg' and __name__ == '__main__':
+    main()
