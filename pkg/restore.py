5b501c28634acd56213d617c9870137fe9d7f965
Colt Baldridge <coltbaldridge@gmail.com>
2021-01-10 13:48:03 -0600

    Move restore.py to the /pkg level

--- pkg/restore.py
+++ pkg/restore.py
@@ -7,13 +7,16 @@ from shutil import copytree
 
 def main():
     
-    # Define essential paths`
-    lab = Path('tests/lab')
-    backup = Path('tests/docs')
+    # Define essential paths.
+    lab = Path('test/lab')
+    backup = Path('test/docs')
 
+    # Remove test/lab and then create a new copy.
     if lab.exists():
         rmtree(lab)
     copytree(backup, lab)
+
+    # Deploy the latest version of script.py to the lab.
     copy2('script.py', lab)
 
 if __name__ == '__main__':
