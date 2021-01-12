46e3965dd90d416d878621647610947a80e077fb
Colt Baldridge <coltbaldridge@gmail.com>
2021-01-12 16:28:04 -0600

    Add messages to display progress

--- pkg/restore.py
+++ pkg/restore.py
@@ -3,6 +3,7 @@ from pathlib import Path
 from shutil import rmtree
 from shutil import copy2
 from shutil import copytree
+from time import sleep
 
 
 def main():
@@ -12,9 +13,17 @@ def main():
     backup = Path('test/docs')
 
     # Remove test/lab and then create a new copy.
+    print('Detecting test/lab', end='')
+    sleep(0.5)
     if lab.exists():
         rmtree(lab)
+    print('Done.')
+
+    # Create a new lab folder.
+    print('Restoring from backup...', end='')
     copytree(backup, lab)
+    print('Done.')
+    sleep(0.5)
 
     # Deploy the latest version of script.py to the lab.
     copy2('script.py', lab)
